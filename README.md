5099b9d0df1b87a27f34a819686b831f4aca5154
Feng Zhang <71230102+anti31415@users.noreply.github.com>
2020-11-07 21:28:18 -0500

    update image and schema as requested.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # homework7_Sql
- 
+ERD Diagram image output using tools provided by quickdatabasediagrams.com
+SQL schema is to prepare datasets by SQL tool.
+Complete the first analysis under visual_data_analysis.ipynb in Starter_Files Folder. 
+
--- schema.sql
+++ schema.sql
@@ -9,7 +9,6 @@ DROP TABLE IF EXISTS transaction;
 CREATE TABLE card_holder 
 (
 	id INT PRIMARY KEY,
-	FOREIGN KEY (id) REFERENCES credit_card(id_card_holder),
 	name VARCHAR(255)
 );
 
@@ -17,15 +16,14 @@ CREATE TABLE card_holder
 CREATE TABLE credit_card
 (
 	card VARCHAR(25) PRIMARY KEY,
-	FOREIGN KEY (card) REFERENCES transaction(card),
-	id_card_holder VARCHAR(255)
+	id_card_holder INT,
+	FOREIGN KEY (id_card_holder) REFERENCES card_holder(id)
 );
 
 -- Create second normal form `family` table
 CREATE TABLE merchant_category
 (
 	id INT PRIMARY KEY,
-	FOREIGN KEY (id) REFERENCES merchant(id_merchant_category),
 	name VARCHAR(255)
 );
 
@@ -33,9 +31,9 @@ CREATE TABLE merchant_category
 CREATE TABLE merchant
 (
 	id INT PRIMARY KEY,
-	FOREIGN KEY (id) REFERENCES transaction(id_merchant),
 	name VARCHAR(255),
-	id_merchant_category VARCHAR(255)
+	id_merchant_category int,
+	FOREIGN KEY (id_merchant_category) REFERENCES merchant_category(id)
 );
 
 -- Create raw data for third normal form use case
@@ -43,12 +41,45 @@ CREATE TABLE transaction
 (
 	id INT PRIMARY KEY,
 	date timestamp without time zone DEFAULT now() NOT NULL,
-	amount VARCHAR(255),
+	amount numeric(8,2) NOT NULL,
 	card VARCHAR(25),
-	id_merchant INT
+	FOREIGN KEY (card) REFERENCES credit_card(card),
+	id_merchant INT,
+	FOREIGN KEY (id_merchant) REFERENCES merchant(id)
 );
 
+-- Create a new table card_info with cardholder_id and card number
+/* select * from transaction*/
+DROP TABLE IF EXISTS card_info;
+create table card_info as select 
+  a.id as cardholder_id
+, a.name
+, b.card
+from card_holder a
+left join credit_card b on a.id = b.id_card_holder;
 
-/* select * from transaction */
+-- Create a new table merchant_info with merchant id and merchant_category
+create table merchant_info as select 
+  a.id as id_merchant
+, a.name
+, b.id as id_merchant_cat
+, b.name as merchant_category
+from merchant a
+left join merchant_category b on a.id_merchant_category = b.id;
 
+select * from card_info
+
+select b.cardholder_id, a.date, sum(amount) FROM transaction a
+left join card_info b on a.card = b.card
+where b.cardholder_id =2 or b.cardholder_id =18
+group by b.cardholder_id, a.date
+order by b.cardholder_id, a.date
+;
+
+select b.cardholder_id, a.date, sum(amount) as amount FROM transaction a
+left join card_info b on a.card = b.card
+where b.cardholder_id =25 and a.date < '2018-07-01'
+group by b.cardholder_id, a.date
+order by a.date
+;
 
